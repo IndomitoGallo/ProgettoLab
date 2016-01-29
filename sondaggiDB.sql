ca720d24add109cdda9b4b1b966d22531ea9670e
IndomitoGallo <lucatalocci@gmail.com>
2016-01-29 18:49:45 +0100

    DataBase Validato Bernacchi (DEFINITIVO)

--- sondaggiDB.sql
+++ sondaggiDB.sql
@@ -3,7 +3,8 @@ CREATE DATABASE IF NOT EXISTS sondaggi;
 USE sondaggi;
 
 CREATE TABLE user (
-	username varchar(30) PRIMARY KEY,
+	id int auto_increment PRIMARY KEY,
+	username varchar(30) not null,
 	nome varchar(20) not null,
 	cognome varchar(20) not null,
 	password varchar(10) not null, 
@@ -20,42 +21,43 @@ CREATE TABLE sondaggio (
 ) Engine=InnoDB;
 
 CREATE TABLE categoria (
-	nome varchar(30) PRIMARY KEY
+	id int auto_increment PRIMARY KEY,
+	nome varchar(30) not null 
 ) Engine=InnoDB;
 
 CREATE TABLE categorieSondaggio (
-	id int,
-	nome varchar(30),
-	PRIMARY KEY (id, nome),
-	FOREIGN KEY (id) REFERENCES sondaggio(id)
+	idSondaggio int,
+	idCategoria int,
+	PRIMARY KEY (idSondaggio, idCategoria),
+	FOREIGN KEY (idSondaggio) REFERENCES sondaggio(id)
 		ON DELETE CASCADE
     	ON UPDATE CASCADE,
-    FOREIGN KEY (nome) REFERENCES categoria(nome)
+    FOREIGN KEY (idCategoria) REFERENCES categoria(id)
 		ON DELETE CASCADE
     	ON UPDATE CASCADE
-)Engine=InnoDB;
+) Engine=InnoDB;
 
 CREATE TABLE categorieUser (
-	username varchar(30),
-	nome varchar(30),
-	PRIMARY KEY (username, nome),
-	FOREIGN KEY (username) REFERENCES user(username)
+	idUser int,
+	idCategoria int,
+	PRIMARY KEY (idUser, idCategoria),
+	FOREIGN KEY (idUser) REFERENCES user(id)
 		ON DELETE CASCADE
     	ON UPDATE CASCADE,
-	FOREIGN KEY (nome) REFERENCES categoria(nome)
+	FOREIGN KEY (idCategoria) REFERENCES categoria(id)
 		ON DELETE CASCADE
     	ON UPDATE CASCADE
 ) Engine=InnoDB;
 
 CREATE TABLE risposta (
-	username varchar(30),
-	id int,
+	idUser int,
+	idSondaggio int,
 	risposta varchar(20),
-	PRIMARY KEY (username, id),
-	FOREIGN KEY (username) REFERENCES user(username)
+	PRIMARY KEY (idUser, idSondaggio),
+	FOREIGN KEY (idUser) REFERENCES user(id)
 		ON DELETE CASCADE
     	ON UPDATE CASCADE,
-    FOREIGN KEY (id) REFERENCES sondaggio(id)
+    FOREIGN KEY (idSondaggio) REFERENCES sondaggio(id)
 		ON DELETE CASCADE
     	ON UPDATE CASCADE
 ) Engine=InnoDB;
