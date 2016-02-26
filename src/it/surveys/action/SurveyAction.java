b698a99d7d9b9d2a3d54cfe33623e0f3fdeab827
LukCame <l.camerlengo94@gmail.com>
2016-02-26 20:01:38 +0100

    Completamento classi SurveyManager e SurveyAction
    Le suddette classi sono state ultimate. Ripeto qui che piu o meno la mia
    parte "Controller-Model" dovrebbe essere finita. Invito quindi Davide
    almeno a completare la classe CategoryDCS poichè senza di quella non è
    possibile realizzare la parte manager ed action.

--- src/it/surveys/action/SurveyAction.java
+++ src/it/surveys/action/SurveyAction.java
@@ -1,9 +1,218 @@
 package it.surveys.action;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
-
+import it.surveys.domain.Survey;
+import it.surveys.model.SurveyManager;
+import java.util.Map;
+/**
+ * Questa classe e' la "action" che gestisce tutte le attivita' che riguardano i sondaggi.
+ * Essa e' un'estensione del Controller e fa da ponte tra le azioni client-side dell'utente
+ * e le operazioni della logica applicativa.
+ * Ogni suo metodo rappresenta una funzione dell'applicazione. 
+ * @author L.Camerlengo
+ * @version 1.0,26/02/16
+ */
 public class SurveyAction extends ActionSupport{
 	
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String question;
+    private String[] answers=new String[4];
+    private int[] categories;
+    private String answer;
+    private String message;
+    private String output;
+    
+    /**
+     * Il metodo createSurvey() viene attivato dopo che il responsabile ha effettuato l'accesso nella
+     * sua pagina personale ed ha cliccato su "Crea sondaggio".
+     * @return String esito della creazione di un determinato sondaggio. 
+     */
+    public String createSurvey(){
+        if(validateSurvey()==false){
+            setMessage("Non sono stati inseriti correttamente tutti i campi obligatori.");
+            return "fail";
+        }
+        SurveyManager sm=SurveyManager.getSurveyManager();
+        Survey s=new Survey();
+        s.setQuestion(question);
+        s.setAnswers(answers);
+        String result=sm.insert(s, categories);
+        if(result.equals("db_fail")){
+            setMessage("Non è stato possibile creare il sondaggio.");
+            return "fail";
+        }
+        return "success";
+    }
+    /**
+     * Il metodo deleteSurvey() viene attivato se dopo che il responsabile ha effettuato l'accesso
+     * nella sua pagina personale e dopo che ha effettuato un click sul pulsante "Cancella" di un determinato
+     * sondaggio, esso ha clicclato su "Ok" nel relativo alert di conferma. 
+     * @return String esito della cancellazione del sondaggio nel database.
+     */
+    public String deleteSurvey(){
+        SurveyManager sm=SurveyManager.getSurveyManager();
+        Survey s=new Survey();
+        s.setId(id);
+        String result=sm.delete(s);
+        if(result.equals("db_fail")){
+            setMessage("Non è stato possibile effettuare la cancellazione del sondaggio.");
+            return "fail";
+        }
+        return "success";
+    }
+    
+    /**
+     * Il metodo displayResults() viene attivato dopo che il responsabile ha effettuato l'accesso nella sua 
+     * pagina personale ed ha effettuato un click sul pulsante "Risultati" di un determinato sondaggio.
+     * @return String esito del prelevamento dei risultati di un determinato sondaggio.
+     */
+    public String displayResults(){
+        SurveyManager sm=SurveyManager.getSurveyManager();
+        String result=sm.displayResults(id);
+        if(result.equals("db_fail")){
+            setMessage("Non è stato possibile prelevare i risultati del sondaggio.");
+            return "fail";
+        }
+        setOutput(result);
+        return "success";
+    }
+    
+    /**
+     * Il metodo displayCreatedSurveys() viene attivato dopo che il responsabile ha effettuato l'accesso nella
+     * sua pagina personale.
+     * @return String esito del prelevamento dei sondaggi creati. 
+     */
+    public String displayCreatedSurveys(){
+        SurveyManager sm=SurveyManager.getSurveyManager();
+        String result=sm.displayCreatedSurveys();
+        if(result.equals("db_fail")){
+            setMessage("Non è stato possibile prelevare i sondaggi creati.");
+            return "fail";
+        }
+        setOutput(result);
+        return "success";
+    }
+    
+    /**
+     * il metodo displayAllowedSurveys() viene attivato quando il cliente ha effettuato l'accesso nella sua
+     * pagina personale.
+     * @return String esito del prelevamento dei sondaggi preferiti del cliente. 
+     */
+    public String displayAllowedSurveys(){
+        SurveyManager sm=SurveyManager.getSurveyManager();
+        Map<String, Object> session = ActionContext.getContext().getSession();
+        String result=sm.displayAllowedSurveys((int)session.get("idUser"));
+        if(result.equals("db_fail")){
+            setMessage("Non è stato possibile prelevare i tuoi sondaggi di interesse.");
+            return "fail";
+        }
+        setOutput(result);
+        return "success";
+    }
+    /**
+     * Il metodo displaySurvey() viene attivato dopo che il cliente ha effettuato l'accesso nella sua pagina
+     * personale ed ha effettuato un click sul pulsante "Visualizza" di un determinato sondaggio.
+     * @return String esito del prelevamento dei dati di un determinato sondaggio. 
+     */
+    public String displaySurvey(){
+        SurveyManager sm=SurveyManager.getSurveyManager();
+        Survey s=new Survey();
+        s.setId(id);
+        String result=sm.retrieve(s);
+        if(result.equals("db_fail")){
+            setMessage("Non è stato possibile visualizzare il sondaggio selezionato.");
+            return "fail";
+        }
+        return "success";
+    }
+    
+    /**
+     * Il metodo answerSurvey() viene attivato dopo che il cliente ha visualizzato un determinato sondaggio con le relative
+     * risposte, ha selezionato una risposta ed ha effettuato un click sul pulsante "Rispondi".
+     * @return String esito dell'inserimento della risposta.
+     */
+    public String answerSurvey(){
+        SurveyManager sm=SurveyManager.getSurveyManager();
+        Map<String, Object> session = ActionContext.getContext().getSession();
+        String result=sm.insertAnswer(id,(int)session.get("idUser"), answer);
+        if(result.equals("db_fail")){
+            setMessage("Non è stato possibile inserire la tua risposta.");
+            return "fail";
+        }
+        return "success";
+    }
+    
+    /**
+     * Il metodo validateSurvey() effettua un controllo dei campi required in fase di creazione del sondaggio da parte del responsabile.
+     * Restituisce false se è vuoto almeno uno dei seguenti campi required: la domanda del sondaggio, la categoria
+     * alla quale associare il sondaggio e le minimo due risposte da associare al sondaggio. Restituisce true altrimenti. 
+     * @return boolean esito della validazione 
+     */
+    private boolean validateSurvey(){
+        if(question.isEmpty()||(answers[0].isEmpty()||answers[1].isEmpty())||categories.length<1)
+            return false;
+        return true;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(String[] answers) {
+        this.answers = answers;
+    }
+
+    public int[] getCategories() {
+        return categories;
+    }
+
+    public void setCategories(int[] categories) {
+        this.categories = categories;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+        
+        
+        
 	
 }
--- src/it/surveys/model/SurveyManager.java
+++ src/it/surveys/model/SurveyManager.java
@@ -1,5 +1,171 @@
 package it.surveys.model;
 
+import it.surveys.domain.Survey;
+
+/**
+ * Questa classe e' il cuore del model e gestisce tutte le azioni che riguardano i sondaggi,
+ * difatti offre i suoi servizi alla SurveyAction.
+ * Il manager, poi, utilizza i servizi del secondo livello del model: le classi DAO e DCS.
+ * Saranno queste ultime a dialogare con il Database.
+ * Infine il manager e' stato implementato come una factory con pattern singleton.
+ * @author L.Camerlengo
+ * @version 1.0 26/02/16
+ */
 public class SurveyManager {
+    
+    private static SurveyManager sm;
+    
+    /**
+     * Il metodo e' l'implementazione del pattern singleton per avere una sola
+     * istanza della classe. Quando viene chiamato per la prima volta istanzia
+     * e ritorna un nuovo oggetto di tipo SurveyManager, mentre le volte successive,
+     * ritorna l'oggetto che e' stato istanziato la prima volta e che e' stato
+     * salvato in un attributo privato e statico, {@link #sm}.
+     * @return sm l'oggetto singleton SurveyManager 
+     */
+    public static SurveyManager getSurveyManager(){
+        if(sm==null)
+            sm=new SurveyManager();
+        return sm;      
+    }
+    
+    /**
+     * Questo metodo chiama due metodi dei livelli inferiori per effettuare l'inserimento del sondaggio sul
+     * database; In particolare si inserisce prima il sondaggio nel database e se tutto va a buon fine si inseriscono
+     * le relative associazioni con le categorie.
+     * Restituisce "success" se l'inserimento del sondaggio con le relative associazioni con le categorie sono state
+     * inserite correttamente nel database, "db_fail" altrimenti.
+     * @param s Survey
+     * @param categories int[]
+     * @return String esito dell'inserimento
+     */
+    public String insert(Survey s,int[] categories){
+        String result=SurveyDAO.insert(s);
+        if(result.equals("fail")){
+            return "db_fail";
+        }
+        result=SurveyDCS.insertCategoryAssociation(s.getId(), categories);
+        if(result.equals("fail")){
+            return "db_fail";
+        }
+        return "success";
+    }
+    
+    /**
+     * Questo metodo utilizzando il corrispondente del livello inferiore in surveyDAO, crea una tabella 
+     * sotto forma di stringa contenente la domanda del sondaggio e tutte le possibili risposte selezionabili dal cliente per rispondere ad
+     * un determinato sondaggio.
+     * Restituisce un tabella sotto forma di stringa contenente la domanda e le possibili risposte selezionabili di un determinato sondaggio se
+     * il metodo retrieve del livello inferiore ha esito positivo, "db_fail" altrimenti.
+     * @param s Survey
+     * @return String tabella contenente la domanda e le possibili risposte selezionabili di un determinato sondaggio.
+     */
+    public String retrieve(Survey s){
+        String result=SurveyDAO.retrieve(s);
+        String[] answers=s.getAnswers();
+        if(result.equals("fail")||answers.length<2){
+            return "db_fail";
+        } 
+        result="<table class=\"withform\">"+
+                "<tr>"+
+                    "<th>Sondaggio</th>"+
+                "</tr>";
+        result=result+"<tr>"+
+                          "<td>"+s.getQuestion()+"</td>"+
+               "</tr>";
+        if(answers.length==2){
+            result=result+"<tr><td><s:form id=\"answer_form\" name=\"answer\" action=\"answerSurvey\" method=\"POST\">"+
+                   "<s:radio class=\"form-control\" id=\"answer\" name=\"answer\" label=\"Risposte\"list=\"# {'risposta1':'"+answers[0]+"',"+
+                   "'risposta2':'"+answers[1]+"'}\"value=\"'risposta1'\"/>";
+        }else if(answers.length==3){
+            result=result+"<tr><td><s:form id=\"answer_form\" name=\"answer\" action=\"answerSurvey\" method=\"POST\">"+
+                   "<s:radio class=\"form-control\" id=\"answer\" name=\"answer\" label=\"Risposte\"list=\"# {'risposta1':'"+answers[0]+"',"+
+                   "'risposta2':'"+answers[1]+"','risposta3':'"+answers[2]+"'}\"value=\"'risposta1'\"/>";
+        }else{
+            result=result+"<tr><td><s:form id=\"answer_form\" name=\"answer\" action=\"answerSurvey\" method=\"POST\">"+
+                   "<s:radio class=\"form-control\" id=\"answer\" name=\"answer\" label=\"Risposte\"list=\"# {'risposta1':'"+answers[0]+"',"+
+                   "'risposta2':'"+answers[1]+"','risposta3':'"+answers[2]+"','risposta4':'"+answers[3]+"'}\"value=\"'risposta1'\"/>";
+        }
+        result=result+"<s:submit class=\"btn\" value=\"Rispondi\"/>"+
+               "</s:form></td></tr>";
+        return result+"</table>";
+    }
+    
+    /**
+     * Questo metodo chiama semplicemente il corrispondente del livello inferiore in SurveyDAO.
+     * Se la cancellazione del sondaggio è andata a buon fine restituisce "success", "db_fail" altrimenti.
+     * @param s Survey
+     * @return String esito della cancellazione del sondaggio
+     */
+    public String delete(Survey s){
+        String result=SurveyDAO.delete(s);
+        if(result.equals("fail")){
+            return "db_fail";
+        }
+        return "success";
+    }
+    
+    /**
+     * Questo metodo chiama semplicemente il corrispondente del livello inferiore in SUrveyDCS.
+     * Restituisce una tabella sotto forma di stringa contenente i risultati di un determinato sondaggio,
+     * altrimenti se qualcosa è andato storto restituisce "db_fail".
+     * @param idSurvey int
+     * @return Risultati di un determinato sondaggio
+     */
+    public String displayResults(int idSurvey){
+        String result=SurveyDCS.displayResults(idSurvey);
+        if(result.equals("fail")){
+            return "db_fail";
+        }
+        return result;
+    }
+    
+    /**
+     * Questo metodo chiama semplicemente il corrispondente del livello inferiore in SUrveyDCS.
+     * Restituisce una tabella sotto forma di stringa contenente i sondaggi presenti nel database, altrimenti
+     * se non è presente alcun sondaggio nel database viene restiuito un messaggio sotto forma di stringa
+     * che notifica al responsabile che non sono presenti sondaggi, altrimenti se qualcosa è andato storto restituisce "db_fail".
+     * @return String sondaggi presenti nel database.
+     */
+    public String displayCreatedSurveys(){
+        String result=SurveyDCS.displayCreatedSurveys();
+        if(result.equals("fail")){
+            return "db_fail";
+        }
+        return result;
+    }
+    
+    /**
+     * Questo metodo chiama semplicemente il corrispondente del livello inferiore in SUrveyDCS.
+     * Restituisce una tabella sotto forma di stringa contenente i sondaggi appartenenti alle categoire di interesse per l'utente presenti nel 
+     * database, altrimenti se non è presente alcun sondaggio associato alle categorie di interesse dell'utente restiuisce un
+     * messaggio sotto forma di stringa, che invita l'utente ad aggiungere nuove categorie di interesse, altrimenti se qualcosa è
+     * andato storto restituisce "db_fail".
+     * @param idUser int
+     * @return String Sondaggi delle relative categorie preferite dell'utente. 
+     */
+    public String displayAllowedSurveys(int idUser){
+        String result=SurveyDCS.displayAllowedSurvey(idUser);
+        if(result.equals("fail")){
+            return "db_fail";
+        }
+        return result;
+    }
+    
+    /**
+     * Questo metodo chiama semplicemente il corrispondente del livello inferiore in SUrveyDCS.
+     * Restituisce "success" se l'inserimento della risposta è andato a buon fine, "db_fail" altrimenti.
+     * @param idSurvey int
+     * @param idUser int
+     * @param answer String
+     * @return String esito dell'inserimento della risposta
+     */
+    public String insertAnswer(int idSurvey,int idUser,String answer){
+        String result=SurveyDCS.insertAnswer(idSurvey, idUser, answer);
+        if(result.equals("fail")){
+            return "db_fail";
+        }
+        return result;
+    }
 
 }
