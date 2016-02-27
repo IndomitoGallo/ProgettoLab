e2e8049937787ecc077d103149c21dfad2da3f36
Davz33 <davide.vitiello33@gmail.com>
2016-02-27 17:59:24 +0100

    Completate la classe CategoryDCS (no JavaDoc)
    Ciò che manca sono 1) Javadoc, a breve
    2)l'HTML non ha la sintassi di struts quindi non è possibile rendere
    operativo il codice , infatti manca s:hidden value che costituisce il
    tramite con i metodi dell'action CategoryDCS

--- src/it/surveys/model/CategoryDCS.java
+++ src/it/surveys/model/CategoryDCS.java
@@ -1,14 +1,15 @@
-package it.categories.model;
+package it.surveys.model;
 
-import it.categories.util.UtilDB;
+import it.surveys.util.UtilDB;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class CategoryDCS {
 
-    public String verifyCategory(String name) {
+    public static String verifyCategory(String name) {
         it.surveys.util.UtilDB utl = it.surveys.util.UtilDB.getUtilDB();
         Connection conn = null;
         Statement stmt = null;
@@ -49,12 +50,12 @@ public class CategoryDCS {
         }
     }
 
-    public String displayListCategories() {
+    public static String displayListCategories() {
 
         it.surveys.util.UtilDB utl = it.surveys.util.UtilDB.getUtilDB();
         Connection conn = null;
         Statement stmt = null;
-        String categories = null;     
+        String categories = null;
         try {
             conn = utl.createConnection();
             stmt = utl.createStatement(conn);
@@ -98,17 +99,156 @@ public class CategoryDCS {
         }
         return categories;
     }
-    
 
-    public String displayRadioCategories() {
+    public static String displayRadioCategories() {
+        it.surveys.util.UtilDB utl = it.surveys.util.UtilDB.getUtilDB();
+        Connection conn = null;
+        Statement stmt = null;
+        String radioCategories = null;
+        try {
+            conn = utl.createConnection();
+            stmt = utl.createStatement(conn);
 
+            String sql = "SELECT name FROM category";
+            //memorizzazione del risultato della query in un ResultSet
+            ResultSet rs = utl.query(stmt, sql);
+            if (!rs.next()) {
+                return "<p>Non sono presenti categorie.</p>";
+            }
+            radioCategories = "<form action=\"..\\CategoryAction\">";
+
+            rs.beforeFirst();
+            String categories;
+            while (rs.next()) {
+                categories = rs.getString(1);
+                radioCategories = radioCategories + "<input type=\"radio\" name=\"" + categories + "\" value=\"" + categories + "\">" + categories + "<br>";
+            }
+            radioCategories = radioCategories + "<input type=\"submit\" value=\"Submit\"></form>";;
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver Not Found!");
+            e.printStackTrace();
+            return "fail";
+        } catch (SQLException e) {
+            System.err.println("Database Error!");
+            e.printStackTrace();
+            return "fail";
+        } finally {
+            try {
+                if (stmt != null) {
+                    utl.closeStatement(stmt);
+                }
+                if (conn != null) {
+                    utl.closeConnection(conn);
+                }
+            } catch (SQLException e) {
+                System.err.println("Close Resource Error!");
+                e.printStackTrace();
+                return "fail";
+            }
+        }
+        return radioCategories;
     }
 
-    public String displayCheckBoxCategories() {
+    public static String displayCheckBoxCategories() {
+        it.surveys.util.UtilDB utl = it.surveys.util.UtilDB.getUtilDB();
+        Connection conn = null;
+        Statement stmt = null;
+        String checkBoxCategories = null;
+        try {
+            conn = utl.createConnection();
+            stmt = utl.createStatement(conn);
 
+            String sql = "SELECT name FROM category";
+            //memorizzazione del risultato della query in un ResultSet
+            ResultSet rs = utl.query(stmt, sql);
+            if (!rs.next()) {
+                return "<p>Non sono presenti categorie.</p>";
+            }
+            checkBoxCategories = "<form action=\"..\\CategoryAction\">";
+
+            rs.beforeFirst();
+            String categories;
+            while (rs.next()) {
+                categories = rs.getString(1);
+                checkBoxCategories = checkBoxCategories + "<input type=\"checkbox\" name=\"" + categories + "\" value=\"" + categories + "\">" + categories + "<br>";
+            }
+            checkBoxCategories = checkBoxCategories + "<input type=\"submit\" value=\"Submit\"></form>";
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver Not Found!");
+            e.printStackTrace();
+            return "fail";
+        } catch (SQLException e) {
+            System.err.println("Database Error!");
+            e.printStackTrace();
+            return "fail";
+        } finally {
+            try {
+                if (stmt != null) {
+                    utl.closeStatement(stmt);
+                }
+                if (conn != null) {
+                    utl.closeConnection(conn);
+                }
+            } catch (SQLException e) {
+                System.err.println("Close Resource Error!");
+                e.printStackTrace();
+                return "fail";
+            }
+        }
+        return checkBoxCategories;
     }
 
-    public String displayCheckBoxCategories(int userCategories[]) {
+    public static String displayCheckBoxCategories(ArrayList<Integer> userCategories) {
+        it.surveys.util.UtilDB utl = it.surveys.util.UtilDB.getUtilDB();
+        Connection conn = null;
+        Statement stmt = null;
+        String checkBoxCategories = null;
+        try {
+            conn = utl.createConnection();
+            stmt = utl.createStatement(conn);
 
+            String sql = "SELECT name FROM category WHERE id IN (";
+            int i = 0;
+            for (; i < userCategories.size() - 1; i++) {
+                sql = sql + userCategories.get(i) + ",";
+            }
+            sql = sql + userCategories.get(i) + ");";
+            //memorizzazione del risultato della query in un ResultSet
+            ResultSet rs = utl.query(stmt, sql);
+            if (!rs.next()) {
+                return "<p>Non sono presenti categorie.</p>";
+            }
+            checkBoxCategories = "<form action=\"..\\CategoryAction\">";
+
+            rs.beforeFirst();
+            String categories;
+            while (rs.next()) {
+                categories = rs.getString(1);
+                checkBoxCategories = checkBoxCategories + "<input type=\"checkbox\" name=\"" + categories + "\" value=\"" + categories + "\">" + categories + "<br>";
+            }
+            checkBoxCategories = checkBoxCategories + "<input type=\"submit\" value=\"Submit\"></form>";
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver Not Found!");
+            e.printStackTrace();
+            return "fail";
+        } catch (SQLException e) {
+            System.err.println("Database Error!");
+            e.printStackTrace();
+            return "fail";
+        } finally {
+            try {
+                if (stmt != null) {
+                    utl.closeStatement(stmt);
+                }
+                if (conn != null) {
+                    utl.closeConnection(conn);
+                }
+            } catch (SQLException e) {
+                System.err.println("Close Resource Error!");
+                e.printStackTrace();
+                return "fail";
+            }
+        }
+        return checkBoxCategories;
     }
 }
